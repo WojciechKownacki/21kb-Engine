3595ff9117504ccbdd194be8039e840808c0830b
Verthsoft <verthsoft@gmail.com>
2026-05-30 00:53:44 +0200

    editor: add native shell and vendor engine dependencies

---
+++ third_party/flecs/src/tree_spawner.h
@@ -0,0 +1,24 @@
+/**
+ * @file tree_spawner.h
+ * @brief Data structure used to speed up the creation of hierarchies.
+ */
+
+#ifndef FLECS_TREE_SPAWNER_H
+#define FLECS_TREE_SPAWNER_H
+
+/* Called during bootstrap to register spawner entities with the world. */
+void flecs_bootstrap_spawner(
+    ecs_world_t *world);
+
+EcsTreeSpawner* flecs_prefab_spawner_build(
+    ecs_world_t *world,
+    ecs_entity_t base);
+
+void flecs_spawner_instantiate(
+    ecs_world_t *world,
+    EcsTreeSpawner *spawner,
+    ecs_entity_t base,
+    ecs_entity_t instance,
+    const ecs_instantiate_ctx_t *ctx);
+
+#endif
