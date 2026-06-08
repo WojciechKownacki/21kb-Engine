e058513bb7354db3b1eddd66b6181637683239d8
Verthsoft <verthsoft@gmail.com>
2026-06-08 22:11:44 +0200

    Harden project descriptors and editor chrome

--- sources/engine/src/private/project/ProjectDescriptorFormat.hpp
+++ sources/engine/src/private/project/ProjectDescriptorFormat.hpp
@@ -1,24 +1,19 @@
 #pragma once
 
+#include <array>
+#include <cstdint>
 #include <string_view>
 
 namespace kb::project {
 
 struct ProjectDescriptorFormat {
-    static constexpr std::string_view Header = "21kb.project.v1";
+    static constexpr std::array<std::uint8_t, 8U> Magic{ '2', '1', 'K', 'B', 'P', 'R', 'J', 0 };
+    static constexpr std::array<std::uint8_t, 8U> MetaMagic{ '2', '1', 'K', 'B', 'P', 'M', 'T', 0 };
     static constexpr std::string_view Extension = ".21kbproject";
-
-    static constexpr std::string_view FileVersionKey = "fileVersion";
-    static constexpr std::string_view EngineAssociationKey = "engineAssociation";
-    static constexpr std::string_view NameKey = "name";
-    static constexpr std::string_view CategoryKey = "category";
-    static constexpr std::string_view DescriptionKey = "description";
-    static constexpr std::string_view ContentRootKey = "contentRoot";
-    static constexpr std::string_view DefaultSceneKey = "defaultScene";
-    static constexpr std::string_view DisableEnginePluginsByDefaultKey = "disableEnginePluginsByDefault";
-    static constexpr std::string_view TargetPlatformsCountKey = "targetPlatforms.count";
-    static constexpr std::string_view ModulesCountKey = "modules.count";
-    static constexpr std::string_view PluginsCountKey = "plugins.count";
+    static constexpr std::uint32_t MaxStringBytes = 1U << 20U;
+    static constexpr std::uint32_t MaxTargetPlatformCount = 256U;
+    static constexpr std::uint32_t MaxModuleCount = 4096U;
+    static constexpr std::uint32_t MaxPluginCount = 4096U;
 };
 
 } // namespace kb::project
