6de5cfc4076c29a1c662dbf2009f050b0e09a47b
Verthsoft <verthsoft@gmail.com>
2026-06-10 17:28:55 +0200

    Refactor: extract Win32InputKeyMap from Win32InputCollector
    Split the VK / XInput-button <-> InputKey mapping tables and reverse lookup
    into a dedicated Win32InputKeyMap (single source of truth), leaving
    Win32InputCollector responsible only for polling devices into InputDeviceState.
    The inspector key-capture now reads the map directly. Pure code move; covered
    by editor unit tests.

--- sources/editor/src/platform/win32/Win32InputCollector.cpp
+++ sources/editor/src/platform/win32/Win32InputCollector.cpp
@@ -4,70 +4,14 @@
 
 #include "engine/input/InputDeviceState.hpp"
 #include "engine/input/InputKey.hpp"
+#include "platform/win32/Win32InputKeyMap.hpp"
 
 #include <Xinput.h>
-
-#include <array>
-#include <cstdint>
 
 namespace kb::editor {
 namespace {
 
 using kb::input::InputKey;
-
-struct KeyBinding {
-    InputKey key;
-    int virtualKey;
-};
-
-// Digital key -> Win32 virtual key. Letters/digits use their ASCII codes.
-constexpr std::array<KeyBinding, 70> kKeyBindings{{
-    {InputKey::A, 'A'}, {InputKey::B, 'B'}, {InputKey::C, 'C'}, {InputKey::D, 'D'},
-    {InputKey::E, 'E'}, {InputKey::F, 'F'}, {InputKey::G, 'G'}, {InputKey::H, 'H'},
-    {InputKey::I, 'I'}, {InputKey::J, 'J'}, {InputKey::K, 'K'}, {InputKey::L, 'L'},
-    {InputKey::M, 'M'}, {InputKey::N, 'N'}, {InputKey::O, 'O'}, {InputKey::P, 'P'},
-    {InputKey::Q, 'Q'}, {InputKey::R, 'R'}, {InputKey::S, 'S'}, {InputKey::T, 'T'},
-    {InputKey::U, 'U'}, {InputKey::V, 'V'}, {InputKey::W, 'W'}, {InputKey::X, 'X'},
-    {InputKey::Y, 'Y'}, {InputKey::Z, 'Z'},
-    {InputKey::Num0, '0'}, {InputKey::Num1, '1'}, {InputKey::Num2, '2'}, {InputKey::Num3, '3'},
-    {InputKey::Num4, '4'}, {InputKey::Num5, '5'}, {InputKey::Num6, '6'}, {InputKey::Num7, '7'},
-    {InputKey::Num8, '8'}, {InputKey::Num9, '9'},
-    {InputKey::F1, VK_F1}, {InputKey::F2, VK_F2}, {InputKey::F3, VK_F3}, {InputKey::F4, VK_F4},
-    {InputKey::F5, VK_F5}, {InputKey::F6, VK_F6}, {InputKey::F7, VK_F7}, {InputKey::F8, VK_F8},
-    {InputKey::F9, VK_F9}, {InputKey::F10, VK_F10}, {InputKey::F11, VK_F11}, {InputKey::F12, VK_F12},
-    {InputKey::Escape, VK_ESCAPE}, {InputKey::Tab, VK_TAB}, {InputKey::Space, VK_SPACE},
-    {InputKey::Enter, VK_RETURN}, {InputKey::Backspace, VK_BACK}, {InputKey::Delete, VK_DELETE},
-    {InputKey::ArrowUp, VK_UP}, {InputKey::ArrowDown, VK_DOWN},
-    {InputKey::ArrowLeft, VK_LEFT}, {InputKey::ArrowRight, VK_RIGHT},
-    {InputKey::LeftShift, VK_LSHIFT}, {InputKey::RightShift, VK_RSHIFT},
-    {InputKey::LeftControl, VK_LCONTROL}, {InputKey::RightControl, VK_RCONTROL},
-    {InputKey::LeftAlt, VK_LMENU}, {InputKey::RightAlt, VK_RMENU},
-    {InputKey::MouseLeft, VK_LBUTTON}, {InputKey::MouseRight, VK_RBUTTON},
-    {InputKey::MouseMiddle, VK_MBUTTON}, {InputKey::MouseThumb1, VK_XBUTTON1},
-    {InputKey::MouseThumb2, VK_XBUTTON2},
-}};
-
-struct GamepadButtonBinding {
-    InputKey key;
-    WORD mask;
-};
-
-constexpr std::array<GamepadButtonBinding, 14> kGamepadButtons{{
-    {InputKey::GamepadFaceBottom, XINPUT_GAMEPAD_A},
-    {InputKey::GamepadFaceRight, XINPUT_GAMEPAD_B},
-    {InputKey::GamepadFaceLeft, XINPUT_GAMEPAD_X},
-    {InputKey::GamepadFaceTop, XINPUT_GAMEPAD_Y},
-    {InputKey::GamepadDPadUp, XINPUT_GAMEPAD_DPAD_UP},
-    {InputKey::GamepadDPadDown, XINPUT_GAMEPAD_DPAD_DOWN},
-    {InputKey::GamepadDPadLeft, XINPUT_GAMEPAD_DPAD_LEFT},
-    {InputKey::GamepadDPadRight, XINPUT_GAMEPAD_DPAD_RIGHT},
-    {InputKey::GamepadLeftShoulder, XINPUT_GAMEPAD_LEFT_SHOULDER},
-    {InputKey::GamepadRightShoulder, XINPUT_GAMEPAD_RIGHT_SHOULDER},
-    {InputKey::GamepadLeftThumb, XINPUT_GAMEPAD_LEFT_THUMB},
-    {InputKey::GamepadRightThumb, XINPUT_GAMEPAD_RIGHT_THUMB},
-    {InputKey::GamepadStart, XINPUT_GAMEPAD_START},
-    {InputKey::GamepadBack, XINPUT_GAMEPAD_BACK},
-}};
 
 [[nodiscard]] bool KeyDown(int virtualKey) noexcept {
     return (GetAsyncKeyState(virtualKey) & 0x8000) != 0;
@@ -98,7 +42,7 @@ void CollectGamepad(kb::input::InputDeviceState& state) noexcept {
         return;
     }
     const XINPUT_GAMEPAD& pad = gamepadState.Gamepad;
-    for (const GamepadButtonBinding& binding : kGamepadButtons) {
+    for (const Win32GamepadButtonBinding& binding : Win32InputKeyMap::GamepadButtons()) {
         state.SetKeyDown(binding.key, (pad.wButtons & binding.mask) != 0);
     }
     state.SetAnalog(InputKey::GamepadLeftStickX, NormalizeStick(pad.sThumbLX));
@@ -111,15 +55,6 @@ void CollectGamepad(kb::input::InputDeviceState& state) noexcept {
 
 } // namespace
 
-kb::input::InputKey Win32InputKeyFromVirtualKey(int virtualKey) noexcept {
-    for (const KeyBinding& binding : kKeyBindings) {
-        if (binding.virtualKey == virtualKey) {
-            return binding.key;
-        }
-    }
-    return InputKey::None;
-}
-
 void Win32InputCollector::Collect(kb::input::InputDeviceState& state, HWND editorWindow) noexcept {
     state.Reset();
     if (!EditorIsForeground(editorWindow)) {
@@ -127,7 +62,7 @@ void Win32InputCollector::Collect(kb::input::InputDeviceState& state, HWND edito
         return;
     }
 
-    for (const KeyBinding& binding : kKeyBindings) {
+    for (const Win32KeyBinding& binding : Win32InputKeyMap::KeyboardAndMouse()) {
         state.SetKeyDown(binding.key, KeyDown(binding.virtualKey));
     }
 
---
+++ sources/editor/src/platform/win32/Win32InputKeyMap.cpp
@@ -0,0 +1,84 @@
+#include "platform/win32/Win32InputKeyMap.hpp"
+
+#if defined(_WIN32)
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <Windows.h>
+#include <Xinput.h>
+
+#include "engine/input/InputKey.hpp"
+
+#include <array>
+
+namespace kb::editor {
+namespace {
+
+using kb::input::InputKey;
+
+// Digital key -> Win32 virtual key. Letters/digits use their ASCII codes.
+constexpr std::array<Win32KeyBinding, 70> kKeyboardAndMouse{{
+    {InputKey::A, 'A'}, {InputKey::B, 'B'}, {InputKey::C, 'C'}, {InputKey::D, 'D'},
+    {InputKey::E, 'E'}, {InputKey::F, 'F'}, {InputKey::G, 'G'}, {InputKey::H, 'H'},
+    {InputKey::I, 'I'}, {InputKey::J, 'J'}, {InputKey::K, 'K'}, {InputKey::L, 'L'},
+    {InputKey::M, 'M'}, {InputKey::N, 'N'}, {InputKey::O, 'O'}, {InputKey::P, 'P'},
+    {InputKey::Q, 'Q'}, {InputKey::R, 'R'}, {InputKey::S, 'S'}, {InputKey::T, 'T'},
+    {InputKey::U, 'U'}, {InputKey::V, 'V'}, {InputKey::W, 'W'}, {InputKey::X, 'X'},
+    {InputKey::Y, 'Y'}, {InputKey::Z, 'Z'},
+    {InputKey::Num0, '0'}, {InputKey::Num1, '1'}, {InputKey::Num2, '2'}, {InputKey::Num3, '3'},
+    {InputKey::Num4, '4'}, {InputKey::Num5, '5'}, {InputKey::Num6, '6'}, {InputKey::Num7, '7'},
+    {InputKey::Num8, '8'}, {InputKey::Num9, '9'},
+    {InputKey::F1, VK_F1}, {InputKey::F2, VK_F2}, {InputKey::F3, VK_F3}, {InputKey::F4, VK_F4},
+    {InputKey::F5, VK_F5}, {InputKey::F6, VK_F6}, {InputKey::F7, VK_F7}, {InputKey::F8, VK_F8},
+    {InputKey::F9, VK_F9}, {InputKey::F10, VK_F10}, {InputKey::F11, VK_F11}, {InputKey::F12, VK_F12},
+    {InputKey::Escape, VK_ESCAPE}, {InputKey::Tab, VK_TAB}, {InputKey::Space, VK_SPACE},
+    {InputKey::Enter, VK_RETURN}, {InputKey::Backspace, VK_BACK}, {InputKey::Delete, VK_DELETE},
+    {InputKey::ArrowUp, VK_UP}, {InputKey::ArrowDown, VK_DOWN},
+    {InputKey::ArrowLeft, VK_LEFT}, {InputKey::ArrowRight, VK_RIGHT},
+    {InputKey::LeftShift, VK_LSHIFT}, {InputKey::RightShift, VK_RSHIFT},
+    {InputKey::LeftControl, VK_LCONTROL}, {InputKey::RightControl, VK_RCONTROL},
+    {InputKey::LeftAlt, VK_LMENU}, {InputKey::RightAlt, VK_RMENU},
+    {InputKey::MouseLeft, VK_LBUTTON}, {InputKey::MouseRight, VK_RBUTTON},
+    {InputKey::MouseMiddle, VK_MBUTTON}, {InputKey::MouseThumb1, VK_XBUTTON1},
+    {InputKey::MouseThumb2, VK_XBUTTON2},
+}};
+
+constexpr std::array<Win32GamepadButtonBinding, 14> kGamepadButtons{{
+    {InputKey::GamepadFaceBottom, XINPUT_GAMEPAD_A},
+    {InputKey::GamepadFaceRight, XINPUT_GAMEPAD_B},
+    {InputKey::GamepadFaceLeft, XINPUT_GAMEPAD_X},
+    {InputKey::GamepadFaceTop, XINPUT_GAMEPAD_Y},
+    {InputKey::GamepadDPadUp, XINPUT_GAMEPAD_DPAD_UP},
+    {InputKey::GamepadDPadDown, XINPUT_GAMEPAD_DPAD_DOWN},
+    {InputKey::GamepadDPadLeft, XINPUT_GAMEPAD_DPAD_LEFT},
+    {InputKey::GamepadDPadRight, XINPUT_GAMEPAD_DPAD_RIGHT},
+    {InputKey::GamepadLeftShoulder, XINPUT_GAMEPAD_LEFT_SHOULDER},
+    {InputKey::GamepadRightShoulder, XINPUT_GAMEPAD_RIGHT_SHOULDER},
+    {InputKey::GamepadLeftThumb, XINPUT_GAMEPAD_LEFT_THUMB},
+    {InputKey::GamepadRightThumb, XINPUT_GAMEPAD_RIGHT_THUMB},
+    {InputKey::GamepadStart, XINPUT_GAMEPAD_START},
+    {InputKey::GamepadBack, XINPUT_GAMEPAD_BACK},
+}};
+
+} // namespace
+
+std::span<const Win32KeyBinding> Win32InputKeyMap::KeyboardAndMouse() noexcept {
+    return kKeyboardAndMouse;
+}
+
+std::span<const Win32GamepadButtonBinding> Win32InputKeyMap::GamepadButtons() noexcept {
+    return kGamepadButtons;
+}
+
+kb::input::InputKey Win32InputKeyMap::InputKeyForVirtualKey(int virtualKey) noexcept {
+    for (const Win32KeyBinding& binding : kKeyboardAndMouse) {
+        if (binding.virtualKey == virtualKey) {
+            return binding.key;
+        }
+    }
+    return InputKey::None;
+}
+
+} // namespace kb::editor
+
+#endif
