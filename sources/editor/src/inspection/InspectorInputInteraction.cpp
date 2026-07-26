c9e329ab5d5a7591dd6b46a7e4d90ea5d691a02c
Verthsoft <verthsoft@gmail.com>
2026-07-26 02:28:50 +0200

    Integrate Win32 input into standalone runtime

--- sources/editor/src/inspection/InspectorInputInteraction.cpp
+++ sources/editor/src/inspection/InspectorInputInteraction.cpp
@@ -6,7 +6,7 @@
 #include "engine/input/InputActionAsset.hpp"
 #include "engine/input/InputKey.hpp"
 #include "engine/input/InputMappingContextAsset.hpp"
-#include "platform/win32/Win32InputKeyMap.hpp"
+#include "engine/platform/win32/Win32InputKeyMap.hpp"
 
 #include <array>
 #include <cstddef>
@@ -102,7 +102,7 @@ bool InspectorInputInteraction::HandleKeyCapture(EditorSceneContext& sceneContex
         inspector.EndKeyCapture();
         return true;
     }
-    const kb::input::InputKey key = Win32InputKeyMap::InputKeyForVirtualKey(static_cast<int>(virtualKey));
+    const kb::input::InputKey key = kb::input::Win32InputKeyMap::InputKeyForVirtualKey(static_cast<int>(virtualKey));
     const int index = inspector.KeyCaptureMappingIndex();
     if (key != kb::input::InputKey::None && index >= 0 &&
         sceneContext.AssetBrowser().SelectionKind() == EditorAssetBrowserSelectionKind::Asset) {
